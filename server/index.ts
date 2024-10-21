edfb6a0a1869466457063125c986f1995eee6b8a
camse <camsemailacc@gmail.com>
2024-10-22 01:16:18 +1100

    asd

--- server/index.ts
+++ server/index.ts
@@ -23,7 +23,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // CORS configuration
 if (process.env.NODE_ENV === 'production') {
