aec1520e42d6ca5b54acf79fa207fd83d1b10a5d
camse <camsemailacc@gmail.com>
2024-10-25 01:00:03 +1100

    asd

--- server/index.ts
+++ server/index.ts
@@ -7,12 +7,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Debug middleware
-app.use((req, _res, next) => {
-  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
-  next();
-});
-
 app.use(cors());
 app.use(express.json());
 
@@ -23,12 +17,13 @@ app.get('/api/test', (_req, res) => {
 });
 
 // Serve static files from root
-app.use(express.static(path.join(__dirname, '..')));
-
-// Catch-all route
-app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../')));
+  
+  app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
