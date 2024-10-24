1c4209ae7084ba5fc6781ff2cfe378090388b5e9
camse <camsemailacc@gmail.com>
2024-10-24 15:39:41 +1100

    modified:   azure-pipelines.yml 	modified:   package.json 	modified:   server/index.ts 	modified:   web.config

--- server/index.ts
+++ server/index.ts
@@ -18,10 +18,10 @@ app.get('/api/test', (_req, res) => {
 
 // Production static file serving
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client')));
+  app.use(express.static(path.join(__dirname, '../')));
   
   app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
   });
 }
 
