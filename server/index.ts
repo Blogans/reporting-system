699e930f71c479661febe67aa615a387696449af
camse <camsemailacc@gmail.com>
2024-10-25 01:09:18 +1100

    asd

--- server/index.ts
+++ server/index.ts
@@ -2,26 +2,26 @@ import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
 import path from 'path';
-
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
 // Test API endpoint
 app.get('/api/test', (_req, res) => {
-  console.log('Test endpoint hit');
   res.json({ message: 'API is working!' });
 });
 
-// Serve static files from root
+// Production static file serving
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../')));
+  app.use(express.static(path.join(__dirname, '../client')));
   
   app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, '../client/index.html'));
   });
 }
 
