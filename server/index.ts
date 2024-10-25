ab2d1567df90c621451e46a329eee32594e21ca7
camse <camsemailacc@gmail.com>
2024-10-26 04:34:12 +1100

    asd

--- server/index.ts
+++ server/index.ts
@@ -6,25 +6,7 @@ import venueRoutes from './routes/venue.route';
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:8080',
-  'https://incident-report-system-g4dtfwhwegdvc7ah.australiaeast-01.azurewebsites.net',
-  '*'
-];
-
-app.use(cors({
-  origin: function(origin, callback) {
-    // allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error('The CORS policy for this site does not allow access from the specified Origin.'), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}));
+app.use(cors());
 
 app.use('/api/venues', venueRoutes);
 
