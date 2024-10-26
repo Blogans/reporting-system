34290efe09dbab72ceff7cb589c07483b8880fab
camse <camsemailacc@gmail.com>
2024-10-26 15:02:33 +1100

    asd

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(cors({
   origin: '*',
--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
   const [response, setResponse] = useState('');
 
   useEffect(() => {
-    fetch(`/api/ping`)
+    fetch(`https://incident-report-system-g4dtfwhwegdvc7ah.australiaeast-01.azurewebsites.net/api/ping`)
       .then(res => res.json())
       .then(data => setResponse(data.message))
       .catch(_err => setResponse('Error connecting to server'));
