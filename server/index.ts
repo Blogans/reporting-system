882577657c78477159a63f004f5c4b6b72ea81fc
camse <camsemailacc@gmail.com>
2024-10-26 19:04:10 +1100

    asd

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '/')));
 
 // Middleware
 const allowedOrigins = [
