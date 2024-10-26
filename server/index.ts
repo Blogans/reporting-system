cddd3f9271ab8e148d2bb0fbe8c551a8a503ffbb
camse <camsemailacc@gmail.com>
2024-10-26 14:13:08 +1100

    asd

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,16 @@
 // index.ts
 import express from 'express';
 import cors from 'cors';
+import dashboardRoutes from './routes/dashboard.route';
 
 const app = express();
 const port = 8080;
+
 app.use(cors({
   origin: '*',
 }));
 
+app.use('/api/dashboard', dashboardRoutes);
 
 app.get('/api/ping', (_req, res) => {
   res.json({ message: 'pong' });
