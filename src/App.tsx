372ad265f9065113b429ea87e865eb796b1563bc
camse <camsemailacc@gmail.com>
2024-10-26 11:58:45 +1100

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState('Loading...')
 
   useEffect(() => {
-    fetch('http://localhost:8080/api/test')
+    fetch('/api/test')
       .then(res => res.json())
       .then(data => setMessage(data.message))
       .catch(err => {
