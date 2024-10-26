4d33e9de23e4277d3727022ce2cf4c11e419418d
camse <camsemailacc@gmail.com>
2024-10-26 18:58:05 +1100

    asd

--- server/index.ts
+++ server/index.ts
@@ -43,7 +43,7 @@ app.get('/api/ping', (_req: Request, res: Response) => {
 });
 
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 // Start server
